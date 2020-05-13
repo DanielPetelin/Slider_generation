236e29cf5e184579d37c47f9037d0a05a1ca082f
Daniil Petelin <daniilpetelin77@gmail.com>
2020-05-13 17:41:27 +0300

    add card

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const img = [
+    'image1.jpg',
+    'image2.jpg',
+    'image3.jpg',
+    'image4.jpg',
+    'image5.jpg',
+    'image6.jpg',
+    'image7.jpg'
+];
+
+let count = img.length;
+
+function init() {
+    let card = document.createElement('div');
+    card.classList.add('card');
+};
